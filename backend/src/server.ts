3abc384de9f393de29e9ee3fd6f1feab9d8c9a71
patrick pan <z5190738@student.unsw.edu.au>
2021-10-04 13:05:02 +1100

    group into a function in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import Database from './db/database';
+import cors from 'cors';
 
-const app: Application = express();
 const port = process.env.PORT || 5000;
-Database.connect();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('hello world!');
-});
+async function main(): Promise<void> {
+    // Connect to MongoDB
+    await Database.connect();
 
-app.listen(port, () => {
-    console.log(`Server is running at port ${port}`);
-});
+    const app: Application = express();
+    app.use(express.json());
+    app.use(cors());
+    
+    app.get('/', (req: Request, res: Response, next: NextFunction) => {
+        res.send('hello world!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is running at port ${port}`);
+    });
+}
+
+main();
+
+
