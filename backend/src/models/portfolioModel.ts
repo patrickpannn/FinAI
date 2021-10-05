0ea3442c5e6cf599e13cd1f67270c9df2600044d
SlowRobot <z5114667@student.unsw.edu.au>
2021-10-06 00:39:43 +1100

    initial portfolio model

---
+++ backend/src/models/portfolioModel.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from 'mongoose';
+import validator from 'validator';
+import dotenv from 'dotenv';
+dotenv.config();
+
+interface PortfolioInterface extends Document {
+    email: string,
+    name: string
+};
+
+const PortfolioSchema = new Schema<PortfolioInterface>({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value: string): void {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid.');
+            }
+        }
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+});
+
+PortfolioSchema.index({ email: 1, name: 1}, { "unique": true } );
