89802b15a4305bd5f342aecb46e8e8f14e9f7b92
SlowRobot <z5114667@student.unsw.edu.au>
2021-10-08 22:50:40 +1100

    initial attempt at login

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import User from '../models/userModel';
+import bcrypt from 'bcryptjs';
 
 export default class UserController {
     public static signup = async (
@@ -14,6 +15,32 @@ export default class UserController {
             await user.save();
 
             res.status(201).json({ token });
+        } catch (e) {
+            res.status(400).json({ error: 'Bad Request.' });
+        }
+    };
+
+    public static login = async (
+        req: Request,
+        res: Response
+    ): Promise<void> => {
+        try {
+            // Unsure how to reference email or password
+            const inputEmail = req.body.email; 
+            const inputPassword = await bcrypt.hash(req.body.password, 10);
+
+            const query = User.findOne({ email: inputEmail, password: inputPassword } );
+            const user = await query.exec();
+
+            if (!user) {
+                res.status(403).json({ error: 'Incorrect Email or Password' });
+            } else {
+               const token: string = user.generateAuth();
+               user.tokens.push({ token });
+               await User.findOneAndUpdate({ email: inputEmail }, user);
+               res.status(201).json({ token });
+            }
+
         } catch (e) {
             res.status(400).json({ error: 'Bad Request.' });
         }
