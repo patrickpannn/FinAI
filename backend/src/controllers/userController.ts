a1b982c518834776a0b038de606bfc1b0ab44ad8
patrick pan <z5190738@student.unsw.edu.au>
2021-10-25 11:41:21 +1100

    fix linting issue

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import User from '../models/userModel';
-import Portfolio from '../models/portfolioModel';
 import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 import EmailService from '../services/emailService';
