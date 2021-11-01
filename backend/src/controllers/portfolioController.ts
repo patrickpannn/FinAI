d238b639d606cf261c93fbb3ed655325c432915a
Ella <ge.song@student.unsw.edu.au>
2021-11-01 16:26:18 +1100

    list portfolios for user

--- backend/src/controllers/portfolioController.ts
+++ backend/src/controllers/portfolioController.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
 import Portfolio from '../models/portfolioModel';
+import Stock from '../models/stockModel';
+
+//export type StockObject = { ticker: string, averagePrice: number, numUnits: number };
+//export type PortfolioList = { name: string, stocks: Array<StockObject> };
+interface Stock {
+    ticker: string,
+    averagePrice: number,
+    numUnits: number
+};
+
 
 export default class PortfolioController {
     public static create = async (
@@ -35,6 +45,48 @@ export default class PortfolioController {
             res.status(201).json({ response: 'Successful' });
         } catch (e) {
             res.status(400).json({ error: 'Portfolio Bad Request' });
+        }
+    };
+
+    public static list = async (
+        req: Request,
+        res: Response
+    ): Promise<void> => {
+        try {
+
+            if (Object.keys(req.body).length) {
+                throw new Error('Inputs are given but not needed.');
+            }
+
+            const portfolios = await Portfolio.find({ user: req.user._id });
+
+            if (!portfolios.length) {
+                throw new Error('User has no portfolios');
+            }
+
+            let list = [];
+
+            for (let i = 0; i < portfolios.length; i++) {
+                let stockList: Stock[] = [];
+                let portfolioInfo = { name: portfolios[i].name,
+                                      stocks: stockList };
+
+                let stocks = await Stock.find({ portfolio: portfolios[i]._id });
+
+                for (let j = 0; j < stocks.length; j++) {
+                    let stockObj = {
+                        ticker: stocks[j].ticker,
+                        averagePrice: stocks[j].averagePrice,
+                        numUnits: stocks[j].numUnits
+                    };
+                    portfolioInfo.stocks.push(stockObj);
+                };
+                list.push(portfolioInfo);
+            }
+
+            res.status(200).send(list);
+        } catch (e) {
+            res.status(400).json({ error: 'List of Portfolios Bad Request' });
         }
     };
 
