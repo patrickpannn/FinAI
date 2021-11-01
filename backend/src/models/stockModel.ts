574ef9c27d6ac30249fdb37ae9b45af9d69fed31
SlowRobot <z5114667@student.unsw.edu.au>
2021-11-02 00:11:09 +1100

    added change for new stock model information

--- backend/src/models/stockModel.ts
+++ backend/src/models/stockModel.ts
@@ -65,6 +65,7 @@ StockSchema.methods.merge = async function
                 this.numUnits -= amount;
                 const newStock = new Stock({ portfolio: newPortfolioId,
                                             ticker: this.ticker,
+                                            name: this.name,
                                             averagePrice: this.averagePrice,
                                             numUnits: amount });
                 await newStock.save();
