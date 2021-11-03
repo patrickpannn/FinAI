d21031210c75256bfe7263f3a84bbab785033fd8
Andre Matkowski <z5206752@ad.unsw.edu.au>
2021-11-03 20:36:49 +1100

    removed verifyOrder middleware

--- backend/src/middlewares/verifyOrder.ts
+++ backend/src/middlewares/verifyOrder.ts
@@ -26,39 +26,6 @@ enum StockBody {
 
 class VerifyOrder {
 
-    public static async verifyOrder(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> {
-        try {
-            if(Object.keys(req.body).length !== 6)
-            {
-                throw new Error('Invalid input');
-            } 
-            for(var parameter in OrderBody)
-            {
-                if(!Object.keys(req.body).includes(parameter)){
-                    throw new Error("Bad Request");
-                }
-            }
-            const portfolio = await Portfolio.findOne({ 
-                user: req.user.id, name : req.body.portfolio });
-            if(!portfolio)
-            {
-                throw new Error('Portfolio specified doesn\'t exist');
-            }
-            if(!Object.values(Direction).includes(req.body.direction))
-            {
-                throw new Error('Invalid direction provided');
-            }
-            next();
-        } catch (e) {
-            console.log(e);
-            res.status(401).json({ error: 'Order is incorrect' });
-        };
-    };
-
     public static async verifyStock(
             req: Request,
             res: Response,
