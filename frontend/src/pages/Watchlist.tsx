f45929b33b01d9b7ca8fa4411cb633c075f9af28
Andre Matkowski <z5206752@ad.unsw.edu.au>
2021-11-08 17:50:49 +1100

    added stock import to watchlist.tsx

--- frontend/src/pages/Watchlist.tsx
+++ frontend/src/pages/Watchlist.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Main, LeftBar, RightContent, WatchlistTitle, TabContainer, useStyles, StyledTabs, StyledTab } from '../styles/watchlist.style';
 import WatchlistTicker from '../components/WatchlistTicker';
+import Stock from '../components/Stock';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state/index';
