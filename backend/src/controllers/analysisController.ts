d10dbe9d461ae2772cd583aa1dc608bd313b7195
SlowRobot <z5114667@student.unsw.edu.au>
2021-11-13 21:08:59 +1100

    clean up code to make it faster

--- backend/src/controllers/analysisController.ts
+++ backend/src/controllers/analysisController.ts
@@ -3,23 +3,17 @@ import axios from 'axios';
 
 const dividendStock = "OMC";
 
-async function getValue(ticker: String): Promise<number> {
+async function getValue(ticker: String, cashFlow: number): Promise<number> {
 
     try {
-        const stockResponse = await axios.get(
-            `https://finnhub.io/api/v1/stock/metric?symbol=${ticker}&metric=all&token=c5vln0iad3ibtqnna830`);
         const stockQuote = await axios.get(
             `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=c5vln0iad3ibtqnna830`);
-
-        if (!stockResponse.data.metric.freeCashFlowPerShareTTM) {
-            throw new Error("Unable to determine cash flow");
-        }
 
         if (!stockQuote.data.c) {
             throw new Error("Unable to get the stock price")
         }
 
-        const stockValue = stockResponse.data.metric.freeCashFlowPerShareTTM;
+        const stockValue = cashFlow;
         const stockCost = stockQuote.data.c;
 
         const value = (stockCost - stockValue) / stockCost;
@@ -39,20 +33,9 @@ function getFuture(): number {
     return 0.3;
 }
 
-async function getRisk(ticker: String): Promise<number> {
+async function getRisk(ticker: String, stockRiskYear1: number, stockRiskYear2: number): Promise<number> {
 
-    try {
-        const stockResponse = await axios.get(
-            `https://finnhub.io/api/v1/stock/metric?symbol=${ticker}&metric=all&token=c5vln0iad3ibtqnna830`);
-
-        if (!stockResponse.data.series.annual.currentRatio[0] || 
-            !stockResponse.data.series.annual.currentRatio[1]) {
-                throw new Error("Unable to determine risk");
-        }
-
-        const stockRiskYear1 = stockResponse.data.series.annual.currentRatio[0].v;
-        const stockRiskYear2 = stockResponse.data.series.annual.currentRatio[1].v;
-        
+    try {        
         let value = 0.5 + ((stockRiskYear1 - stockRiskYear2) / stockRiskYear2);
 
         if (value > 1) {
@@ -67,21 +50,17 @@ async function getRisk(ticker: String): Promise<number> {
     }
 }
 
-async function getDividend(ticker: String): Promise<number> {
+async function getDividend(ticker: String, stockYield: number): Promise<number> {
 
     try {
-        const stockResponse = await axios.get(
-            `https://finnhub.io/api/v1/stock/metric?symbol=${ticker}&metric=all&token=c5vln0iad3ibtqnna830`);
         const compareResponse = await axios.get(
             `https://finnhub.io/api/v1/stock/metric?symbol=${dividendStock}&metric=all&token=c5vln0iad3ibtqnna830`);
 
 
-        if (!stockResponse.data.metric.dividendYield5Y || 
-            !compareResponse.data.metric.dividendYield5Y) {
+        if (!compareResponse.data.metric.dividendYield5Y) {
                 throw new Error("Unable to determine dividend yield");
         }
 
-        const stockYield = stockResponse.data.metric.dividendYield5Y;
         const comparisonYield = compareResponse.data.metric.dividendYield5Y;
         
         let value = 0.8 + ((stockYield - comparisonYield) / comparisonYield);
@@ -109,12 +88,44 @@ export default class AnalysisController {
                 throw new Error('Incorrect inputs given');
             }
 
+            const stockResponse = await axios.get(
+                `https://finnhub.io/api/v1/stock/metric?symbol=${req.body.ticker}&metric=all&token=c5vln0iad3ibtqnna830`);
+            
+            let valueValue = 0;
+
+            if (!stockResponse.data.metric.freeCashFlowPerShareTTM) {
+                valueValue = -1;
+            } else {
+                const cashFlow = stockResponse.data.metric.freeCashFlowPerShareTTM
+                valueValue = await getValue(req.body.ticker, cashFlow);
+            }
+
+            let riskValue = 0;
+
+            if (!stockResponse.data.series.annual.currentRatio[0] || 
+                !stockResponse.data.series.annual.currentRatio[1]) {
+                    riskValue = -1;
+            } else {
+                const stockRiskYear1 = stockResponse.data.series.annual.currentRatio[0].v;
+                const stockRiskYear2 = stockResponse.data.series.annual.currentRatio[1].v;
+                riskValue = await getRisk(req.body.ticker, stockRiskYear1, stockRiskYear2);
+            }    
+
+            let dividendValue = 0;
+
+            if (!stockResponse.data.metric.dividendYield5Y) {
+                dividendValue = -1;
+            } else {
+                const stockYield = stockResponse.data.metric.dividendYield5Y;
+                dividendValue = await getDividend(req.body.ticker, stockYield);
+            }
+
             const snowflake = {
-                value: await getValue(req.body.ticker),
+                value: valueValue,
                 past: getPast(),
                 future: getFuture(),
-                risk: await getRisk(req.body.ticker),
-                dividend: await getDividend(req.body.ticker)
+                risk: riskValue,
+                dividend: dividendValue
             };
 
             res.status(200).json(snowflake);
