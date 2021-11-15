ddc65209421062110816b5133a24079b05fd9e0b
ella-song-1013 <z5261532@ad.unsw.edu.au>
2021-11-16 00:10:38 +1100

    start limit order frontend

--- frontend/src/components/OrderModal.tsx
+++ frontend/src/components/OrderModal.tsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state/index';
 import StyledDialogTitle from './StyledDialogTitle';
+import { TabContainer, StyledTabs, StyledTab } from '../styles/watchlist.style';
 
 const url = process.env.REACT_APP_URL || 'http://localhost:5000';
 
@@ -20,8 +21,10 @@ const OrderModal: React.FC<Props> = ({ open, ticker, stockName, onClose }) => {
     const styles = useStyles();
     const dispatch = useDispatch();
     const { setToast } = bindActionCreators(actionCreators, dispatch);
+    const [page, setPage] = useState('BUY NOW');
+    const [amount, setAmount] = useState('');
 
-    const handleSubmit = async (
+    const handleNormal = async (
         e: React.FormEvent<HTMLFormElement>
     ): Promise<void> => {
         e.preventDefault();
@@ -59,16 +62,57 @@ const OrderModal: React.FC<Props> = ({ open, ticker, stockName, onClose }) => {
         }
     };
 
+    const handleLimit = async (
+        e: React.FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+        e.preventDefault();
+        try {
+            if (!parseInt(units, 10) || !parseInt(amount, 10)) {
+                console.log(units);
+                console.log(amount);
+                throw new Error('Please enter numbers');
+            } else if (+units <= 0) {
+                throw new Error('Units should be positive');
+            }
+
+            const response = await fetch(`${url}/user/order/buyLimitOrder`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${sessionStorage.getItem('access_token')}`,
+                },
+                body: JSON.stringify({
+                    units: units,
+                    price: amount, 
+                    portfolio: 'Default',
+                    ticker: ticker,
+                    name: stockName
+                }),
+            });
+            if (response.status === 200) {
+                setToast({ type: 'success', message: `Order Executed!` });
+                onClose();
+            } else if (response.status === 401) {
+                throw new Error('Authentication Failed');
+            } else {
+                throw new Error('Insufficient funds');
+            }
+        } catch (error) {
+            setToast({ type: 'error', message: `${error}` });
+        }
+    };
+
     return (
         <BootstrapDialog
             aria-labelledby="buy order"
             open={open}
         >
+            { page === 'BUY NOW' && <>
             <StyledDialogTitle id="buy-title" onClose={onClose}>
-                BUY
+                Buy Now
             </StyledDialogTitle>
             <DialogContent className={styles.container} dividers>
-                <form onSubmit={handleSubmit} className={styles.form}>
+                <form onSubmit={handleNormal} className={styles.form}>
                     <TextField
                         id="outlined-number"
                         label="Units"
@@ -87,6 +131,74 @@ const OrderModal: React.FC<Props> = ({ open, ticker, stockName, onClose }) => {
                     </DialogActions>
                 </form>
             </DialogContent>
+
+            <TabContainer>
+                <StyledTabs
+                    aria-label="tabs"
+                >
+                    <StyledTab
+                        className = {`${page === 'BUY NOW' && styles.selected}`}
+                        onClick={(): void => setPage('BUY NOW')} >
+                        BUY NOW
+                    </StyledTab>
+                    <StyledTab
+                        onClick={(): void => setPage('LIMIT ORDER')} >
+                        BUY LIMIT ORDER
+                    </StyledTab>
+                </StyledTabs>
+            </TabContainer>
+            </>
+            }
+            { page === 'LIMIT ORDER' && 
+            <>
+            <StyledDialogTitle id="buy-title" onClose={onClose}>
+                Limit Order
+            </StyledDialogTitle>
+            <DialogContent className={styles.container} dividers>
+                <form onSubmit={handleLimit} className={styles.form}>
+                    <TextField
+                        label="Expected Price"
+                        type="text"
+                        value={amount}
+                        onChange={
+                            (e): void => setAmount(e.target.value)
+                        }
+                        required
+                        fullWidth
+                    />
+                    <TextField
+                        label="Units"
+                        type="text"
+                        value={units}
+                        onChange={
+                            (e): void => setUnits(e.target.value)
+                        }
+                        required
+                        fullWidth
+                    />
+                    <DialogActions>
+                        <SubmitButton autoFocus type='submit' variant="contained">
+                            Place Limit Order
+                        </SubmitButton>
+                    </DialogActions>
+                </form>
+            </DialogContent>
+                <TabContainer>
+                <StyledTabs
+                    aria-label="tabs"
+                >
+                    <StyledTab
+                        onClick={(): void => setPage('BUY NOW')} >
+                        BUY NOW
+                    </StyledTab>
+                    <StyledTab
+                        className = {`${page === 'LIMIT ORDER' && styles.selected}`}
+                        onClick={(): void => setPage('BUY LIMIT ORDER')} >
+                        BUY LIMIT ORDER
+                    </StyledTab>
+                </StyledTabs>
+            </TabContainer>
+            </>}
         </BootstrapDialog>
     );
 };
