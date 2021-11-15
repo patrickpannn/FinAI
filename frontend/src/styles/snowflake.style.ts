dbd8f9b0d8da51212fd4b6f83ccaa03423aab84d
ella-song-1013 <z5261532@ad.unsw.edu.au>
2021-11-15 13:12:12 +1100

    finished

--- frontend/src/styles/snowflake.style.ts
+++ frontend/src/styles/snowflake.style.ts
@@ -3,7 +3,7 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme: Theme) => createStyles({
     chartContainer: {
         position: "relative",
-        height:'40vh',
-        width:'80vw',
+        height:'300px',
+        width:'300px',
     }
 }));
