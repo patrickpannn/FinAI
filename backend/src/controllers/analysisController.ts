5248d93ae7af963783407626cd5c4c58778b2cce
Andre Matkowski <z5206752@ad.unsw.edu.au>
2021-11-15 21:56:50 +1100

    added gradient calculation to snowflake past

--- backend/src/controllers/analysisController.ts
+++ backend/src/controllers/analysisController.ts
@@ -31,13 +31,62 @@ async function getValue(ticker: String, cashFlow: number): Promise<number> {
 
 }
 
-async function getPast(): Promise<number> {
+async function getPast(
+    ticker: string
+): Promise<number>{
     try{
-        const response = await axios.get(
-            `https://api.twelvedata.com/time_series?symbol=${NASDAQ}&interval=1month&outputsize=36&apikey=614acd00d55849d19a5fca8f5f6ca17a`);
+        /*
+        const indices = [
+            await axios.get(
+                `https://api.twelvedata.com/time_series?symbol=${NASDAQ}&interval=1month&outputsize=12&apikey=614acd00d55849d19a5fca8f5f6ca17a`),
+            await axios.get(
+                `https://api.twelvedata.com/time_series?symbol=${SMP500}&interval=1month&outputsize=12&apikey=614acd00d55849d19a5fca8f5f6ca17a`),
+            await axios.get(
+                `https://api.twelvedata.com/time_series?symbol=${DOWJONES}&interval=1month&outputsize=12&apikey=614acd00d55849d19a5fca8f5f6ca17a`),
+            ] as const;
+        console.log(indices[0].data);
+*/
+        const stockResponse = await axios.get(
+          `https://finnhub.io/api/v1/stock/candle?symbol=${ticker}&resolution=M&from=${Math.floor(Date.now() / 1000 - 31536000)}&to=${Math.floor(Date.now() / 1000)}&token=c5vln0iad3ibtqnna830`);
 
-        const data = response.data;
-        console.log(response.data);
+        const stockPrices = Array(stockResponse.data.c)[0];
+        
+        let sumXY: number = 0.0;
+        let sumY: number = 0.0;
+        let sumX: number = 0.0;
+        let xCounter: number = 1;
+        let sumXSquared: number = 0.0;
+
+
+        stockPrices.forEach((element: number) => {
+            sumXY += element*xCounter;
+            sumY += element;
+            sumX += xCounter;
+            sumXSquared += xCounter^2;
+            xCounter++;
+        });
+        const stockGrad = (stockPrices.length*sumXY-sumY*sumX)/(stockPrices.length*sumXSquared-sumX^2);
+
+
+        // Get index gradient for past year
+        const index = await axios.get(
+            `https://api.twelvedata.com/time_series?symbol=${NASDAQ}&interval=1month&outputsize=12&apikey=614acd00d55849d19a5fca8f5f6ca17a`);
+        const indexValues = index.data.values;
+
+        let indexSumXY: number = 0.0;
+        let indexSumY: number = 0.0;
+        let indexSumX: number = 0.0;
+        let indexXCounter: number = 1;
+        let indexSumXSquared: number = 0.0;
+
+        for(indexXCounter = 1; xCounter < indexValues.length; indexXCounter++){
+            indexSumXY += +indexValues[indexXCounter].close*indexXCounter;
+            indexSumY += +indexValues[indexXCounter].close;
+            indexSumX += indexXCounter;
+            indexSumXSquared += Math.pow(indexXCounter,2);
+        }
+        const indexGrad = (indexValues.length*indexSumXY-indexSumY*indexSumX)/(indexValues.length*indexSumXSquared-indexSumX^2);
+        console.log(indexGrad);
         return 0.2;
 
     } catch(e)
@@ -146,9 +195,11 @@ export default class AnalysisController {
                 dividendValue = await getDividend(stockYield);
             }
 
+            const past = await getPast(req.body.ticker);
+
             const snowflake = {
                 value: valueValue,
-                past: getPast(),
+                past: past,
                 future: getFuture(),
                 risk: riskValue,
                 dividend: dividendValue
