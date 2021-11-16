1f7ae294f946948cc2b0c062ed8ad46bdff11ece
ella-song-1013 <z5261532@ad.unsw.edu.au>
2021-11-16 23:15:50 +1100

    solve merge conflict

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,8 @@ import WatchlistRoute from './routers/watchlistRoute';
 import App from './app';
 import dotenv from 'dotenv';
 import PortfolioRoute from './routers/portfolioRoute';
-import AnalysisRoute from './routers/analysisRoute';
 import OrderRoute from './routers/orderRoute';
-<<<<<<< HEAD
-
-=======
 import AnalysisRoute from './routers/analysisRoute';
->>>>>>> c1d7a27ae4c8f6c87568111f5a63881277d726b8
 dotenv.config();
 
 async function main(): Promise<void> {
@@ -19,11 +14,7 @@ async function main(): Promise<void> {
         new WatchlistRoute(),
         new PortfolioRoute(),
         new OrderRoute(),
-<<<<<<< HEAD
-        new AnalysisRoute()
-=======
         new AnalysisRoute(),
->>>>>>> c1d7a27ae4c8f6c87568111f5a63881277d726b8
     ]);
 
     app.listen();
